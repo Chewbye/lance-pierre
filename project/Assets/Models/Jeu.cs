179d9782c731918a0391465edec750c8ec553d18
Pierre Malmassari <p.malma@hotmail.fr>
2015-02-21 12:52:29 +0100

    Ajout des attributs  _Score_final, _Nb_cible_touchees et _Nb_cible_manquees dans jeu

--- project/Assets/Models/Jeu.cs
+++ project/Assets/Models/Jeu.cs
@@ -26,12 +26,48 @@ public class Jeu{
 		}
 	}
 
+	private int _Score_final;
+	
+	public int Score_final {
+		get {
+			return _Score_final;
+		}
+		set {
+			_Score_final = value;
+		}
+	}
+
+	private int _Nb_cible_touchees;
+	
+	public int Nb_cible_touchees {
+		get {
+			return _Nb_cible_touchees;
+		}
+		set {
+			_Nb_cible_touchees = value;
+		}
+	}
+
+	private int _Nb_cible_manquees;
+	
+	public int Nb_cible_manquees {
+		get {
+			return _Nb_cible_manquees;
+		}
+		set {
+			_Nb_cible_manquees = value;
+		}
+	}
+
 	/**
 	 * Créé un modèle Jeu à partir d'un fichier de configuation
 	 */
 	public Jeu(Conf configuration){
 		_Config = configuration;
 		_Nb_lancers = _Config.Nb_lancers;
+		_Score_final = 0;
+		_Nb_cible_manquees = 0;
+		_Nb_cible_touchees = 0;
 	}
 
 	/**
@@ -40,12 +76,18 @@ public class Jeu{
 	public Jeu(){
 		_Config = new Conf ();
 		_Nb_lancers = _Config.Nb_lancers;
+		_Score_final = 0;
+		_Nb_cible_manquees = 0;
+		_Nb_cible_touchees = 0;
 	}
 
 
 	public override string ToString(){
 		string res = "";
-		res += _Nb_lancers + System.Environment.NewLine;
+		res += _Nb_lancers + System.Environment.NewLine 
+			+ _Score_final + System.Environment.NewLine 
+			+ _Nb_cible_touchees + System.Environment.NewLine 
+			+ _Nb_cible_manquees + System.Environment.NewLine;
 
 		return res;
 	}
