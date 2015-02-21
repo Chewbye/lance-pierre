52a634bb41b9d48aae0d0d9d2c1330d24f4752ac
Pierre Malmassari <p.malma@hotmail.fr>
2015-02-21 19:31:27 +0100

    Tableau de double qui contient pour chaque lancer la distance entre la catapulte et la cible

--- project/Assets/Scripts/PositionCible.cs
+++ project/Assets/Scripts/PositionCible.cs
@@ -4,13 +4,26 @@ using System;
 
 public class PositionCible : MonoBehaviour {
 
+	public GameObject catapulte;
+	private double distance;
+
 	// Use this for initialization
 	void Start () {
+		// On place aléatoirement la cible dans une zone donnée
 		System.Random rnd = new System.Random();
 		int x = rnd.Next(3, 27);
 		int y = rnd.Next(-5, 14); 
-		Vector3 position = new Vector3( x, y, 0 );
-		transform.position = position;
+		Vector3 positionCible = new Vector3( x, y, 0 );
+		transform.position = positionCible;
+
+		// On récupère la position de la catapulte
+		Vector3 positionCatapulte = catapulte.transform.position;
+
+		// On calcule la distance entre la catapulte et la cible
+		distance = Math.Sqrt (Math.Pow(((double)positionCible.x - (double)positionCatapulte.x), 2) + Math.Pow(((double)positionCible.y - (double)positionCatapulte.y), 2));
+
+		// On enregistre la distance dans le tableau des distances
+		GameController.Jeu._Une_distance [GameController.Jeu.Tir_courant] = distance;
 	}
 
 	// Update is called once per frame
