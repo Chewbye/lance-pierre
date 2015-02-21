74994a06bf132051444c0bd5646e81a93ae0ca92
Pierre Malmassari <p.malma@hotmail.fr>
2015-02-21 19:53:39 +0100

    Affichage du compte à rebours (Reset de la scène lorsque 0s)

---
+++ project/Assets/Scripts/CompteARebours.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CompteARebours : MonoBehaviour {
+
+	public float tempsRestant = 15.0f; // En seconde
+	Text txt;
+	
+	// Use this for initialization
+	void Start () {
+		txt = gameObject.GetComponent<Text>(); 
+		txt.text = "Temps restant : " + tempsRestant + " secondes";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		tempsRestant -= Time.deltaTime;
+		txt.text= "Temps restant : " + tempsRestant + " secondes";
+		if (tempsRestant <= 0.0f)
+		{
+			Reset ();
+		}; 
+	}
+
+	void Reset () {
+		// On incrémente le nombre de cibles manquées
+		GameController.Jeu.Nb_cible_manquees ++;
+		
+		// On indique que le tir courant est manqué
+		GameController.Jeu._Un_tir[GameController.Jeu.Tir_courant] = false;
+		
+		// On incrémente le tir courant
+		GameController.Jeu.Tir_courant++;
+		
+		Debug.Log(GameController.Jeu);
+		
+		//	On recharge la meme scène
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
