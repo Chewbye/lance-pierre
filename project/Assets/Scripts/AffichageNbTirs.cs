d7bb4785a18698c5080e230852869ea74ba2a744
Pierre Malmassari <p.malma@hotmail.fr>
2015-02-21 16:24:20 +0100

    Affichage du nombre de tirs dans le jeu

---
+++ project/Assets/Scripts/AffichageNbTirs.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AffichageNbTirs : MonoBehaviour {
+	
+	Text txt;
+	
+	// Use this for initialization
+	void Start () {
+		txt = gameObject.GetComponent<Text>(); 
+		txt.text= GameController.Jeu.Tir_courant + " tir(s) effectué(s) sur " + GameController.Jeu.Nb_lancers;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		txt.text= GameController.Jeu.Tir_courant + " tir(s) effectué(s) sur " + GameController.Jeu.Nb_lancers; 
+	}
+}
---
+++ project/Assets/Scripts/AffichageTirs.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AffichageTirs : MonoBehaviour {
+	
+	Text txt;
+	
+	// Use this for initialization
+	void Start () {
+		txt = gameObject.GetComponent<Text>(); 
+		txt.text= "Tir(s) réussi(s) : " +GameController.Jeu.Nb_cible_touchees + " Tir(s) ratés : " + GameController.Jeu.Nb_cible_manquees;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		txt.text = "Tir(s) réussi(s) : " + GameController.Jeu.Nb_cible_touchees + " Tir(s) ratés : " + GameController.Jeu.Nb_cible_manquees;
+	}
+}
