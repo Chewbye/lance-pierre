36dc2c72b048abf56eb27f96ac429d746e9d1c01
Pierre Malmassari <p.malma@hotmail.fr>
2015-03-07 12:03:00 +0100

    La cible et la balle changent de taille en fonction du calibrage initial

---
+++ project/Assets/Scripts/CalibrageTailleElement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CalibrageTailleElement : MonoBehaviour 
+{
+	public GameObject balle;
+	public GameObject cible;
+
+	// Use this for initialization
+	void Start () 
+	{
+		double ratioEchelle = GameController.Jeu.Config.Ratio_echelle;
+		balle.transform.localScale = new Vector3((float) ratioEchelle, (float)ratioEchelle, (float)ratioEchelle);
+		cible.transform.localScale = new Vector3((float) ratioEchelle, (float)ratioEchelle, (float)ratioEchelle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
