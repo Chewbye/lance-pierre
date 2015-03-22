49370aa7a44b3ba7f8193a2deb391053a95b4c30
Pierre Malmassari <p.malma@hotmail.fr>
2015-03-22 14:41:39 +0100

    Cible touchée : Affichage d'un message

---
+++ project/Assets/Scripts/AffichageGagnePerdu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AffichageGagnePerdu : MonoBehaviour 
+{
+	public GameObject cible;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// CHANGEMENT DE LA POSITION DU TEXTE DE REUSSITE A PROXIMITE DE LA CIBLE
+		Vector3 positionCible = cible.transform.position;
+		double diametreCible = cible.renderer.bounds.size.x;
+
+		transform.position = new Vector3((float) (positionCible.x - 2*diametreCible), positionCible.y, transform.position.z);
+
+		if(GameController.Jeu.Cible_Touchee)
+		{
+			TextMesh textMesh = GetComponent(typeof(TextMesh)) as TextMesh;
+			textMesh.text = "+" + GameController.Jeu.Config.Nb_points_gagnes_par_cible + " points";
+		}
+	}
+}
